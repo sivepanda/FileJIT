5b5e5b5a17d76da7029dc7dcaa86f91e1976b7f1
Siven Panda <sivenpanda@icloud.com>
2025-04-19 20:07:26 -0400

    tauri frontend ini

---
+++ frontend/FolderJIT/src/routes/+page.svelte
@@ -0,0 +1,139 @@
+<script lang="ts">
+  import { open } from "@tauri-apps/plugin-dialog";
+
+  async function selectFile() {
+    const selected = await open({
+      multiple: false,
+      directory: true,
+    });
+
+    if (selected === null) {
+      console.log("No file selected");
+    } else {
+      console.log("Selected file:", selected);
+    }
+  }
+</script>
+
+<main class="container">
+  <h1>Welcome to <span class="highlht">FolderJIT</span></h1>
+
+
+  <button on:click={selectFile}>Get Started</button>
+
+</main>
+
+<style>
+  .highlht {
+    color: #e67f53;
+  }
+  
+  :root {
+    font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 400;
+
+    color: #9b8484;
+    background-color: #534848;
+
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
+  }
+
+  .container {
+    margin: 0;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+  }
+
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: 0.75s;
+  }
+
+  .logo.tauri:hover {
+    filter: drop-shadow(0 0 2em #24c8db);
+  }
+
+  .row {
+    display: flex;
+    justify-content: center;
+  }
+
+  a {
+    font-weight: 500;
+    color: #646cff;
+    text-decoration: inherit;
+  }
+
+  a:hover {
+    color: #535bf2;
+  }
+
+  h1 {
+    text-align: center;
+  }
+
+  input,
+  button {
+    border-radius: 12px;
+    border: 1px solid transparent;
+    width: 50%;
+    align-self: center;
+    padding: 0.6em 1.2em;
+    font-size: 1em;
+    font-weight: 500;
+    font-family: inherit;
+    color: #0f0f0f;
+    background-color: #ffffff;
+    transition: border-color 0.25s;
+    box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  button:hover {
+    border-color: #396cd8;
+  }
+  button:active {
+    border-color: #396cd8;
+    background-color: #e8e8e8;
+  }
+
+  input,
+  button {
+    outline: none;
+  }
+
+
+  @media (prefers-color-scheme: dark) {
+    :root {
+      color: #f6f6f6;
+      background-color: #222422;
+    }
+
+    a:hover {
+      color: #24c8db;
+    }
+
+    input,
+    button {
+      color: #ffffff;
+      background-color: #0f0f0f98;
+    }
+    button:active {
+      background-color: #0f0f0f69;
+    }
+  }
+</style>
