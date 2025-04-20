df0c6892a073dc51184e045ebf7532c18a7ee060
Ahmad Choudhary <asc113@duke.edu>
2025-04-19 21:27:05 -0400

    push

--- backend/main.py
+++ backend/main.py
@@ -3,28 +3,37 @@ import time
 import shutil
 import requests
 from folder import Folder
+from fileClassifier import FileClassifier
 
 autosort_path="./../autosort"
+
+
 def get_new_folder():
     """Watch the autosort directory for a new folder"""
-    print(f"👀 Watching folder: {autosort_path}")
+    while True:
+        print(f"👀 Watching folder: {autosort_path}")
 
-    item_name = None
-    while not item_name:
-        items = [f for f in os.listdir(autosort_path) if os.path.isdir(os.path.join(autosort_path, f))]
-        if items:
-            item_name = items[0]
-            item_path = os.path.join(autosort_path, item_name)
-        else:
-            time.sleep(1)
+        item_name = None
+        while not item_name:
+            print("testing ")
+            items = [f for f in os.listdir(autosort_path)]
+            if items:
+                item_name = items[0]
+                item_path = os.path.join(autosort_path, item_name)
+            else:
+                time.sleep(1)
 
-    folder_path = os.path.join(autosort_path, item_name)
+        folder_path = os.path.join(autosort_path, item_name)
 
-    print(f"\n✅ Folder detected: {item_name}\n")
+        print(f"\n✅ Folder detected: {item_name}\n")
 
-    return folder_path
+        text = Folder()
+        text.generate_text(folder_path)
+        classify = FileClassifier(folder_path)
+        print("iteration done")
 
-classify = Folder()
+get_new_folder()
 
-old_path = get_new_folder()
-classify.generate_text(old_path)
+
+
+
